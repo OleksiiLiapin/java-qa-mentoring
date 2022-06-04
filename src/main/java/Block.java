a452d68a7abfa22a72ebd3e6376d133223646e0d
Oleksii Liapin <49100107+OleksiiLiapin@users.noreply.github.com>
2022-06-04 19:37:28 +0300

    initial commit

--- src/main/java/Block.java
+++ src/main/java/Block.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Block {
+public class Block {
+    private int width;
+    private int length;
+    private int height;
+
+    public Block(int[] walls) {
+        this.width = walls[0];
+        this.length = walls[1];
+        this.height = walls[2];
+    }
+
+    public int getVolume() {
+        return width * length * height;
+    }
+
+    public int getSurfaceArea()  {
+        return (2 * height * width) + (2 * length * height) + (2 * width * length);
+    }
+    //2× h × W + 2× L × h + 2× w × L.
+
+    public int getWidth() {
+
+        return width;
+    }
+
+    public int getLength() {
+
+        return length;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+
 }

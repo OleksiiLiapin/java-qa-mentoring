5c469bafdb7ea08645651610fac0229a676c4810
Oleksii Liapin <49100107+OleksiiLiapin@users.noreply.github.com>
2022-06-05 15:51:58 +0300

    Leap Year task

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        Block b = new Block(new int[]{2,4,6});
-        System.out.println(b.getWidth()); // -> 2);
-        System.out.println( b.getLength()); // -> 4);
-        System.out.println(b.getHeight()); // -> 6);
-        System.out.println(b.getVolume()); // -> 48);
-        System.out.println(b.getSurfaceArea()); // -> 88 );
+//        Block b = new Block(new int[]{2,4,6});
+//        System.out.println(b.getWidth()); // -> 2);
+//        System.out.println( b.getLength()); // -> 4);
+//        System.out.println(b.getHeight()); // -> 6);
+//        System.out.println(b.getVolume()); // -> 48);
+//        System.out.println(b.getSurfaceArea()); // -> 88 );
 
+
+
+        System.out.println("2032 is leap year - " + LeapYear.isLeapYear(2032) );
+        System.out.println("1960 is leap year - " + LeapYear.isLeapYear(1960) );
+        System.out.println("1993 is NOT leap year - " + LeapYear.isLeapYear(1993) );
+        System.out.println("2000 is leap year - " + LeapYear.isLeapYear(2000) );
 
 
 
